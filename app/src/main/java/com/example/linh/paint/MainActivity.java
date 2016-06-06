af8b0cc37ad4d14a81a3e985339eff33363eb4dd
Linh1991 <mr.yagamiraito@gmail.com>
2016-06-06 16:25:51 +0700

    add pick img from gallery and share button

--- app/src/main/java/com/example/linh/paint/MainActivity.java
+++ app/src/main/java/com/example/linh/paint/MainActivity.java
@@ -1,18 +1,169 @@
 package com.example.linh.paint;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import android.support.v7.widget.ShareActionProvider;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     private Button buttonBg;
     private Button buttonPencil;
     private Button buttonErase;
     private Button buttonExport;
+    private SimpleDrawingView drawingView;
+    private File root;
+    private ListView listviewFolder;
+    private TextView textFolder;
+    private Button buttonParentFolder;
+    private File curFolder;
+    private List<String> fileList = new ArrayList<String>();
+    private static final int SELECTED_PICTURE = 1 ;
+    private ShareActionProvider mShareActionProvider;
+    private File imagePath;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        root = new File(Environment.getExternalStorageDirectory().getAbsolutePath());
         setContentView(R.layout.activity_main);
+        drawingView = (SimpleDrawingView) findViewById(R.id.drawing);
+        buttonErase = (Button) findViewById(R.id.buttonEarse);
+        buttonErase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    drawingView.setErase(true);
+            }
+        });
+        buttonPencil = (Button) findViewById(R.id.buttonPencil);
+        buttonPencil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawingView.setErase(false);
+
+            }
+        });
+        buttonExport = (Button) findViewById(R.id.buttonExport);
+        buttonExport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawingView.setDrawingCacheEnabled(true);
+                imagePath = new File(Environment.getExternalStorageDirectory() + "/data/screenshot.png");
+                FileOutputStream fos;
+                try
+                {
+                    Bitmap bitmap = drawingView.getDrawingCache();
+                    fos = new FileOutputStream(imagePath);
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                    fos.flush();
+                    fos.close();
+                    shareIt();
+
+                }
+                catch (FileNotFoundException e)
+                {
+                    Toast unsavedToast = Toast.makeText(getApplicationContext(),
+                            "Oops! Image could not be saved.", Toast.LENGTH_SHORT);
+                    unsavedToast.show();
+                }
+                catch (IOException e)
+                {
+                    Toast unsavedToast = Toast.makeText(getApplicationContext(),
+                            "Oops! Image could not be saved.", Toast.LENGTH_SHORT);
+                    unsavedToast.show();
+                }
+            }
+
+        });
+        buttonBg = (Button) findViewById(R.id.buttonBg);
+        buttonBg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               displayDialogOpenBg();
+            }
+        });
+    }
+
+    private void shareIt() {
+        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+        Uri screenshotUri = Uri.parse(imagePath.getAbsolutePath());
+        sharingIntent.setType("image/jpeg");
+        sharingIntent.putExtra(Intent.EXTRA_STREAM, screenshotUri);
+        startActivity(Intent.createChooser(sharingIntent, "Share image using"));
+
+    }
+
+    private void displayDialogOpenBg() {
+
+                Intent i  = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i,SELECTED_PICTURE);
+
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode)
+        {
+            case SELECTED_PICTURE:
+                if (resultCode == RESULT_OK)
+                {
+
+                    Uri uri = data.getData();
+                    String[]projection={MediaStore.Images.Media.DATA};
+                    Cursor cursor = getContentResolver().query(uri,projection,null,null,null);
+                    cursor.moveToFirst();
+                    int columnIndex = cursor.getColumnIndex(projection[0]);
+                    String filePatch = cursor.getString(columnIndex);
+                    drawingView.startNew();
+                    drawingView.setupDrawing(filePatch);
+                    cursor.close();
+                }
+        }
+    }
+
+    private void listDir(File f) {
+        if(f.equals(root))
+        {
+            buttonParentFolder.setEnabled(false);
+        }
+        else
+        {
+            buttonParentFolder.setEnabled(true);
+        }
+
+        curFolder = f;
+        textFolder.setText(f.getPath());
+        File[] files  = f.listFiles();
+        fileList.clear();
+        for(File file : files)
+        {
+            fileList.add(file.getPath());
+        }
+        ArrayAdapter<String> direciryList = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,fileList);
+        listviewFolder.setAdapter(direciryList);
     }
 }
--- app/src/main/java/com/example/linh/paint/SimpleDrawingView.java
+++ app/src/main/java/com/example/linh/paint/SimpleDrawingView.java
@@ -6,11 +6,18 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Environment;
 import android.util.AttributeSet;
 import android.view.View;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.view.MotionEvent;
 
 import java.io.File;
 
@@ -18,23 +25,107 @@ import java.io.File;
  * Created by linh on 6/6/2016.
  */
 public class SimpleDrawingView extends View {
-    private final int paintColor = Color.BLACK;
     // defines paint and canvas
-    private Paint drawPaint;
-
+    //drawing path
+    private Path drawPath;
+    //drawing and canvas paint
+    private Paint drawPaint, canvasPaint;
+    //initial color
+    private int paintColor = 0xFF660000;
+    //canvas
+    private Canvas drawCanvas;
+    //canvas bitmap
+    private Bitmap canvasBitmap;
+    private File b;
     public SimpleDrawingView(Context context, AttributeSet attrs) {
         super(context, attrs);
-                setupDrawing();
-        File pathName = Environment.getExternalStorageDirectory() ;
-        Resources res = getResources();
-        Bitmap bitmap = BitmapFactory.decodeFile(pathName + "/Download/zzz");
-        BitmapDrawable bd = new BitmapDrawable(res, bitmap);
-        setBackground(bd);
+
+        setupDrawingNull();
+
     }
 
-    private void setupDrawing(){
+    public void startNew(){
+        drawCanvas.drawColor(0, PorterDuff.Mode.CLEAR);
+        invalidate();
+    }
+    private void setupDrawingNull() {
+        drawPath = new Path();
+        drawPaint = new Paint();
+        drawPaint.setColor(paintColor);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(20);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
 
+    }
+
+    public void setupDrawing(String a){
+        Resources res = getResources();
+        Bitmap bitmap = BitmapFactory.decodeFile(a);
+        BitmapDrawable bd = new BitmapDrawable(res, bitmap);
+        setBackground(bd);
+        drawPath = new Path();
+        drawPaint = new Paint();
+        drawPaint.setColor(paintColor);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(20);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
         }
+
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        //view given size
+        super.onSizeChanged(w, h, oldw, oldh);
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        drawCanvas = new Canvas(canvasBitmap);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+    //draw view
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+        canvas.drawPath(drawPath, drawPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //detect user touch
+        float touchX = event.getX();
+        float touchY = event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                drawPath.moveTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                drawPath.lineTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_UP:
+                drawCanvas.drawPath(drawPath, drawPaint);
+                drawPath.reset();
+                break;
+            default:
+                return false;
+        }
+        invalidate();
+        return true;
+    }
+    private boolean erase=false;
+    public void setErase(boolean isErase){
+//set erase true or false
+        erase=isErase;
+        if(erase) drawPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+        else drawPaint.setXfermode(null);
+    }
+    public void setBg()
+    {
+
+    }
     }
 
 
