c8717fd20767ce6071a4b63ef47a300864a68f52
Linh1991 <mr.yagamiraito@gmail.com>
2016-06-07 16:12:31 +0700

    remove simple drawn  add imagaeview

--- app/src/main/java/com/example/linh/paint/MainActivity.java
+++ app/src/main/java/com/example/linh/paint/MainActivity.java
@@ -4,18 +4,21 @@ import android.animation.Animator;
 import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.database.Cursor;
-import android.graphics.Bitmap;
+
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.annotation.ColorInt;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
@@ -36,7 +39,6 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonPencil;
     private Button buttonErase;
     private Button buttonExport;
-    private SimpleDrawingView drawingView;
     private Button buttonChangeBg;
     private Button buttonNewBg;
     private File root;
@@ -45,57 +47,41 @@ public class MainActivity extends AppCompatActivity {
     private LinearLayout menubar;
     ValueAnimator mAnimator;
     boolean changeBG = false;
+    boolean menubariSVisiable = true;
+    RelativeLayout relativeLayout;
+    private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         root = new File(Environment.getExternalStorageDirectory().getAbsolutePath());
         setContentView(R.layout.activity_main);
-        drawingView = (SimpleDrawingView) findViewById(R.id.drawing);
+        relativeLayout = (RelativeLayout                                                                                                        ) findViewById(R.id.test);
+//        drawingView = (SimpleDrawingView) findViewById(R.id.drawing);
         buttonErase = (Button) findViewById(R.id.buttonEarse);
         menubar= (LinearLayout) findViewById(R.id.menubar);
+        TextView textView = new TextView(this);
+        textView.setText("New text");
         buttonErase.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                drawingView.setErase(true);
+
             }
         });
         buttonPencil = (Button) findViewById(R.id.buttonPencil);
         buttonPencil.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new ColorOMaticDialog.Builder()
-                        .initialColor(drawingView.getColor())
-                        .colorMode(ColorMode.ARGB) // RGB, ARGB, HVS
-                        .indicatorMode(IndicatorMode.HEX) // HEX or DECIMAL; Note that using HSV with IndicatorMode.HEX is not recommended
-                        .onColorSelected(new OnColorSelectedListener() {
-                            @Override
-                            public void onColorSelected(@ColorInt int i) {
-                                drawingView.setErase(false);
-                                drawingView.setColor(i);
-                            }
-                        })
-                        .showColorIndicator(true) // Default false, choose to show text indicator showing the current color in HEX or DEC (see images) or not
-                        .create()
-                        .show(getSupportFragmentManager(), "ColorOMaticDialog");
 
             }
         });
         buttonExport = (Button) findViewById(R.id.buttonExport);
-
         buttonExport.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                drawingView.setDrawingCacheEnabled(true);
                 imagePath = new File(Environment.getExternalStorageDirectory() + "/data/screenshot.png");
                 FileOutputStream fos;
                 try
                 {
-                    Bitmap bitmap = drawingView.getDrawingCache();
-                    fos = new FileOutputStream(imagePath);
-                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
-                    fos.flush();
-                    fos.close();
-                    shareIt();
 
                 }
                 catch (FileNotFoundException e)
@@ -117,8 +103,17 @@ public class MainActivity extends AppCompatActivity {
         buttonBg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                menubar.setVisibility(View.VISIBLE);
-                mAnimator.start();
+                if(menubariSVisiable)
+                {
+                    menubar.setVisibility(View.VISIBLE);
+                    mAnimator.start();
+                    menubariSVisiable = false;
+                }
+                else {
+                    collapse();
+                    menubariSVisiable = true;
+                }
+
             }
         });
         menubar.getViewTreeObserver().addOnPreDrawListener(
@@ -148,7 +143,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 changeBG = true;
                 displayDialogOpenBg();
-
             }
         });
         buttonNewBg = (Button) findViewById(R.id.buttonNewBG);
@@ -157,14 +151,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 changeBG = false;
                 displayDialogOpenBg();
-
+                collapse();
             }
         });
     }
     private ValueAnimator slideAnimator(int start, int end) {
-
         ValueAnimator animator = ValueAnimator.ofInt(start, end);
-
         animator.addUpdateListener(new     ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator valueAnimator) {
@@ -207,8 +199,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void shareIt() {
-        drawingView.setDrawingCacheEnabled(true);
-        drawingView.invalidate();
         String path = Environment.getExternalStorageDirectory().toString();
         OutputStream fOut = null;
         File file = new File(path,
@@ -226,16 +216,7 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
 
         }
-        if (drawingView.getDrawingCache() == null) {
-        }
-        drawingView.getDrawingCache()
-                .compress(Bitmap.CompressFormat.JPEG, 85, fOut);
-        try {
-            fOut.flush();
-            fOut.close();
-        } catch (IOException e) {
 
-        }
 
         Intent sharingIntent = new Intent();
         sharingIntent.setAction(Intent.ACTION_SEND);
@@ -253,8 +234,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -271,12 +250,10 @@ public class MainActivity extends AppCompatActivity {
                     String filePatch = cursor.getString(columnIndex);
                     if(changeBG)
                     {
-                        drawingView.changeBg(filePatch);
+
                     }
                     else
                     {
-                        drawingView.startNew();
-                        drawingView.setupDrawing(filePatch);
                     }
                     cursor.close();
                 }
